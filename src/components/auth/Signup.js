e48470134e57b5733ecc6a8e65658a542613bb13
phunmbi <phunmbi@gmail.com>
2018-11-28 11:30:33 +0100

    feature(sign up): process input
    install axios
    process sign up field input

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import validateAuth from '../../lib/validation';
+import Loading from '../shared/Loading';
+import spinner from '../../assets/Spinner-1s.gif';
 
 class Signup extends Component {
   state = {
@@ -22,7 +24,7 @@ class Signup extends Component {
     event.preventDefault();
 
     // Run validation
-    const { firstName, lastName, email, password, confirmPassword } = this.state;
+    const { firstName, lastName, email, password, confirmPassword, errors } = this.state;
     const fieldNames = ['firstname', 'lastname', 'email', 'password', 'confirmPassword'];
     const status = validateAuth({ firstName, lastName, email, password, confirmPassword }, event.target.id, event.target.value, fieldNames);
 
@@ -37,7 +39,22 @@ class Signup extends Component {
       password: password.trim()
     };
 
-    // Set loading icon and request
+    // If there are no validation errors
+    if (!status.status) {
+      // Set loading icon and request
+      event.target.children.submit.innerHTML = ''
+      event.target.children.submit.style.background = `#FEEF6D url(${spinner}) no-repeat center`;
+
+      // Clear input fields
+      event.target.children.firstName.value = '';
+      event.target.children.lastName.value = '';
+      event.target.children.email.value = '';
+      event.target.children.password.value = '';
+      event.target.children.confirmPassword.value = '';
+
+      // Send request
+      console.log(body);
+    }
   }
 
   handleChange = (event) => {
@@ -52,22 +69,22 @@ class Signup extends Component {
     return (
       <div className="hero-account">
         <h3>Create your MyDiary Account</h3>
-            <form onSubmit={this.handleSubmit}>
-                <input onChange={this.handleChange} id="firstName" type="text" placeholder="First Name" required />
-                <span className='errorResponse'>{status.firstname || errors.firstname}</span>
-                <input onChange={this.handleChange} id="lastName" type="text" placeholder="Last Name" required />
-                <span className='errorResponse'>{status.lastname || errors.lastname}</span>
-                <input onChange={this.handleChange} id="email" type="email" placeholder="Email" required />
-                <span className='errorResponse'>{status.email || errors.email}</span>
-                <input onChange={this.handleChange} id="password" type="password" placeholder="Password" required min='8' />
-                <span className='errorResponse'>{status.password || errors.password}</span>
-                <input onChange={this.handleChange} id="confirmPassword" type="password" placeholder="Confirm Password" required min='8' />
-                <span className='errorResponse'>{status.confirmPassword || errors.confirmPassword}</span>
-                <input className="loading" id="submit" type="submit" value="Create Account" />
-            </form>
-            <p>Already have an account? then
-                <a href="index.html"> Sign in</a>
-            </p>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} id="firstName" type="text" placeholder="First Name" required />
+            <span className='errorResponse'>{status.firstname || errors.firstname}</span>
+            <input onChange={this.handleChange} id="lastName" type="text" placeholder="Last Name" required />
+            <span className='errorResponse'>{status.lastname || errors.lastname}</span>
+            <input onChange={this.handleChange} id="email" type="email" placeholder="Email" required />
+            <span className='errorResponse'>{status.email || errors.email}</span>
+            <input onChange={this.handleChange} id="password" type="password" placeholder="Password" required min='8' />
+            <span className='errorResponse'>{status.password || errors.password}</span>
+            <input onChange={this.handleChange} id="confirmPassword" type="password" placeholder="Confirm Password" required min='8' />
+            <span className='errorResponse'>{status.confirmPassword || errors.confirmPassword}</span>
+            <button id="submit" type="submit">Create Account</button>
+          </form>
+          <p>Already have an account? then
+            <a href="index.html"> Sign in</a>
+          </p>
       </div>
     );
   }
