545feebb2e2e54627e53d322e1722cfdc2ecd9a6
phunmbi <phunmbi@gmail.com>
2018-11-28 17:34:38 +0100

    feature(sign in): set up validation

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import validateAuth from '../../lib/validation';
 
 class Signin extends Component {
+  state = {
+    email: '',
+    password: '',
+    errors: {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
   render() {
+    const { firstName, lastName, email, password, confirmPassword, errors } = this.state;
+    const fieldNames = ['firstName', 'lastName', 'email', 'password', 'confirmPassword'];
+    const status = validateAuth({ firstName, lastName, email, password, confirmPassword }, fieldNames);
+
     return (
       <div className="hero">
         <div className="title">
@@ -17,12 +35,20 @@ class Signin extends Component {
           </div>
           <div className="hero-account">
               <h3>Sign in</h3>
+              <p className="errorResponse" id="errorResponse" />
               <form>
-                  <input type="text" placeholder="Email" required />
-                  <input type="text" placeholder="Password" required />
-                  <button id="submit" type="submit">Sign in</button>
+                  <input onChange={this.handleChange} id="email" type="email" placeholder="Email" required />
+                  <span className="errorResponse">
+                    {status.email || errors.email}
+                  </span>
+                  <input onChange={this.handleChange} id="password" type="password" placeholder="Password" required min="8" />
+                  <span className="errorResponse">
+                    {status.password || errors.password}
+                  </span>
+                  <button id="submit" type="submit">
+                    Sign in
+                  </button>
               </form>
-
               <p>Don't have an account? then
                   <Link to="/"> Sign up</Link>
               </p>
